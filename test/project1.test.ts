cde381176afbaec5aedeff493cbbe2ffa98d1d73
dailywoes <johnrteixeira@gmail.com>
2020-11-23 03:29:34 -0500

    2

--- test/project1.test.ts
+++ test/project1.test.ts
@@ -1,21 +1,48 @@
 import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as Project1 from '../lib/project1-stack';
+import * as lambda from '@aws-cdk/aws-lambda';
 
-test('SQS Queue Created', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new Project1.Project1Stack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(haveResource("AWS::SQS::Queue",{
-      VisibilityTimeout: 300
+import { HitCounter } from '../lib/hitcounter';
+
+test('DynamoDB Table Created with Encryption', () => {
+    const stack = new cdk.Stack();
+
+    new HitCounter(stack, 'MyTestConstruct', {
+        downstream: new lambda.Function(stack, 'TestFunction', {
+            runtime: lambda.Runtime.NODEJS_10_X,
+            handler: 'lambda.handler',
+            code: lambda.Code.fromInline('test')
+        })
+    });
+
+    expectCDK(stack).to(haveResource('AWS::DynamoDB::Table', {
+        SSESpecification: {
+            SSEEnabled: true
+        }
     }));
 });
 
-test('SNS Topic Created', () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new Project1.Project1Stack(app, 'MyTestStack');
-  // THEN
-  expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
+test('Lambda has correct variables', () => {
+    const stack = new cdk.Stack();
+
+    new HitCounter(stack, 'MyTestConstruct', {
+        downstream: new lambda.Function(stack, 'TestFunction', {
+            runtime: lambda.Runtime.NODEJS_10_X,
+            handler: 'lambda.handler',
+            code: lambda.Code.fromInline('test')
+        })
+    });
+
+    expectCDK(stack).to(haveResource("AWS::Lambda::Function", {
+        Environment: {
+            Variables: {
+                DOWNSTREAM_FUNCTION_NAME: {
+                    "Ref": "TestFunction22AD90FC"
+                },
+                HITS_TABLE_NAME: {
+                    "Ref": "MyTestConstructHits24A357F0"
+                }
+            }
+        }
+    }));
 });
